1a5f2994d9653bf29b4fb87a53ded16effa0144f
yankuchinsky <yankuchinsky@yandex.ru>
2018-06-16 20:52:55 +0300

    Added volume control

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -1,6 +1,12 @@
 import React, {Component} from 'react';
 import {Songs} from '../api'
 
+const SONG_PROGRESS = '.song-progress-bar.progress-bar > .progress';
+const SONG_AUDIO = '.song-audio';
+const VOLUME = '.volume-progress-bar.progress-bar';
+const VOLUME_PROGRESS =  '.volume-progress-bar.progress-bar > .progress';
+
+
 class MainPage extends Component {
 	state = {
 		songs: [],
@@ -13,8 +19,8 @@ class MainPage extends Component {
 		timer: null
 	}
 	handleSong = (id) => {
-		const audio = document.querySelector('.song-audio');
-		const playerBar = document.querySelector('.player__progress-bar .progress');
+		const audio = document.querySelector(SONG_AUDIO);
+		const playerBar = document.querySelector(SONG_PROGRESS);
 		if(audio){ 
 			if(this.state.play && this.state.currentPlay === id){
 				audio.pause();
@@ -37,11 +43,12 @@ class MainPage extends Component {
 		}
 	}
 	handlePlay = () => {
-		const audio = document.querySelector('.song-audio');
+		const audio = document.querySelector(SONG_AUDIO);
 		if(audio){
 			if(this.state.play && this.state.currentPlay){
 				this.setState({
-					play: false
+					play: false,
+					timer: null
 				}, () => audio.pause());
 			}else{
 				this.setState({
@@ -50,17 +57,33 @@ class MainPage extends Component {
 			}
 		}
 	}
+	changeVolume = (e) => {
+		if(this.state.currentPlay){
+			const volumeBar = document.querySelector(VOLUME);
+			const volumeProgress = document.querySelector(VOLUME_PROGRESS);
+			const audio = document.querySelector(SONG_AUDIO);
+			const volumeBarPosition = e.target.getBoundingClientRect();
+			const clientClickPosition = e.clientX
+			const positionInside = clientClickPosition - volumeBarPosition.x.toFixed(0);
+			const volume = positionInside / volumeBar.clientWidth;
+			volumeProgress.style.width = positionInside + 'px'; 
+			audio.volume = volume;
+		}
+	}
 	changeProgrees = (e) => {
-		let audio = document.querySelector('.song-audio');
-		const playerBar = document.querySelector('.player__progress-bar .progress');
+		console.log('progress changed');
+		let audio = document.querySelector(SONG_AUDIO);
+		const playerBar = document.querySelector(SONG_PROGRESS);
 		const x = e.clientX;
 		const width = document.documentElement.clientWidth; //document.body.clientWidth;
 		const progress = x / width * 100;
 		const audioPercent = audio.duration / 100;
 		const val = audioPercent * progress;
+		console.log(val);
 		audio.currentTime = val;
 		if(this.state.play){
 			this.setState({
+				currentTime: val,
 				timer: this.createTimer(audio, playerBar)
 			});
 		}else{
@@ -119,7 +142,7 @@ class MainPage extends Component {
 			<div className="main-page">
 				{songs}
 				<div className={"player " + playerActive}>
-					<div onClick={(e) => this.changeProgrees(e)} className={"player__progress-bar " + progressBarStatus}>
+					<div onClick={(e) => this.changeProgrees(e)} className="song-progress-bar progress-bar">
 						<div className="progress">
 						</div>
 					</div>
@@ -133,6 +156,9 @@ class MainPage extends Component {
 						</div>
 						<div className="player-button button-next" onClick={() => this.changeSong(currentIndex + 1)}>
 						</div>
+					</div>
+					<div className="volume-progress-bar progress-bar" onClick={(e) => this.changeVolume(e)}>
+						<div className="progress"></div>
 					</div>
 					<audio className ="song-audio" src={ this.state.currentPlay ? "http://127.0.0.1:5000/file/" + this.state.currentPlay : ''} ></audio>
 				</div>
